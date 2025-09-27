7505d4bedc14952406187339ace0de05c98cfd12
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2025-09-27 14:52:46 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# - Create a distance converter converting Km to miles
+# - Take two inputs from user: Their first name and the distance in km
+# - Print: Greet user by name and show km, and mile values
+# - 1 mile is 1.609 kilometers
+# - hint: use correct types for calculating and print
+# - Did you capitalize the name
+
+name = input('enter your name')
+distance_km = input('distance in KM: ')
+distance_mil = float(distance_km)/1.609
+print(f'Hi {name.title()}! {distance_km}km is equivalent to {round(distance_mil, 1)} miles.')
